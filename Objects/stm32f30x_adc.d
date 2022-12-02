ea3ef4b3759e3a63c05de6256144cb6102843dfa
WillyYang <willy.yang@quantatw.com>
2022-12-02 14:05:40 +0800

    template test

---
+++ Objects/stm32f30x_adc.d
@@ -0,0 +1,34 @@
+./objects/stm32f30x_adc.o: \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\src\stm32f30x_adc.c \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_adc.h \
+  Stm32F303_FW\CMSIS\Device\ST\STM32F30x\Include\stm32f30x.h \
+  Stm32F303_FW\CMSIS\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  Stm32F303_FW\CMSIS\Include\core_cmInstr.h \
+  Stm32F303_FW\CMSIS\Include\core_cmFunc.h \
+  Stm32F303_FW\CMSIS\Include\core_cmSimd.h \
+  Stm32F303_FW\CMSIS\Device\ST\STM32F30x\Include\system_stm32f30x.h \
+  User\stm32f30x_conf.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_can.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_crc.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_comp.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_dac.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_dbgmcu.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_dma.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_exti.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_flash.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_fmc.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_gpio.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_hrtim.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_syscfg.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_i2c.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_iwdg.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_opamp.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_pwr.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_rcc.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_rtc.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_spi.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_tim.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_usart.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_wwdg.h \
+  Stm32F303_FW\STM32F30x_StdPeriph_Driver\inc\stm32f30x_misc.h
